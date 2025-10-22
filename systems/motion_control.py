67cc11e2fc373565eb86b959d6f3cb5eced5b4b3
Advay Chandra <advay.chandra@protonmail.com>
2025-10-22 12:15:28 -0400

    Rename MotorController to MotionController

--- systems/motion_control.py
+++ systems/motion_control.py
@@ -1,9 +1,9 @@
 from buildhat import Motor, MotorPair
 
-class Motion:
+class MotionController:
   def __init__(self, **kwargs):
     motors = MotorPair(kwargs["motors"][0], kwargs["motors"][1])
     motorLeft = Motor(kwargs["motors"][0])
     motorRight = Motor(kwargs["motors"][1])
-    max_speed = kwargs["max_speed"]
-    
+    max_speed = kwargs.get("max_speed", 30)
+  
