de91a9fc89f54cdea29b6a1a3ece033f0665f78b
AdvayRChandra <advay.chandra@protonmail.com>
2025-10-25 21:44:42 -0400

    Fixed poc1

--- poc/proof_of_concept.py
+++ poc/proof_of_concept.py
@@ -1,5 +1,6 @@
 from buildhat import MotorPair, Motor
 from basehat import LineFinder
+import time
 
 def main():
         # Initialize the motor pair on ports A and B
@@ -10,26 +11,27 @@ def main():
         direction = 0
         while True:
                 sensor_value = linefinder.value
-                print("Line Finder Sensor Value:", sensor_value)
+                print(f"Line Finder Sensor Value: {sensor_value} Direction:{direction}")
                 if sensor_value == 0:
                         if direction == 0:
-                                motor_pair.start(-50, 0)  # Turn right
+                                motor_pair.start(-10, 0)  # Turn right
                                 direction = 1
                         elif direction > 0:
-                                if direction < 50:
+                                if direction < 10:
                                         direction += 1
                                 else:
                                         direction = -1
-                                        motor_pair.start(0, 50)  # Turn left
+                                        motor_pair.start(-10, 0)  # Turn left
                         else:
-                                if direction > -50:
+                                if direction > -10:
                                         direction -= 1
                                 else:
                                         direction = 1
-                                        motor_pair.start(-50, 0)  # Turn right
+                                        motor_pair.start(0, 10)  # Turn right
                 else:
                         direction = 0
-                        motor_pair.start(-50, 50)  # Move forward
+                        motor_pair.start(-10, 10)  # Move forward
+                time.sleep(0.1)
 
 if __name__ == "__main__":
         main()
