5deb38cfc6961d604b1cfc2a4a088abe14b54186
AdvayRChandra <advay.chandra@protonmail.com>
2025-12-04 11:09:57 -0500

    Add systems/__init__.py with module exports

---
+++ systems/__init__.py
@@ -0,0 +1,12 @@
+# MARCO core systems modules
+from .mobility_system import MotionController
+from .navigation_system import Location
+from .sensors import SensorInput
+
+# Note: task_manager.py is currently empty
+
+__all__ = [
+    'MotionController',
+    'Location',
+    'SensorInput',
+]
